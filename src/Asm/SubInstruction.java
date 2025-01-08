76ba6d7a653af9e3789863543815cacf4a4c086a
Amin - Ayoub <ayoub.nadir@etu.univlyon1.fr>
2025-01-08 09:33:22 +0100

    fix

--- src/Asm/SubInstruction.java
+++ src/Asm/SubInstruction.java
@@ -1,5 +1,6 @@
 package src.Asm;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -29,5 +30,16 @@ public class SubInstruction extends Instruction {
 
     public void setLiveVariables(List<String> liveVariables) {
         this.liveVariables = liveVariables;
+    }
+
+    @Override
+    public String toAssembly(HashMap<String, String> registerAllocation) {
+        StringBuilder assembly = new StringBuilder();
+        assembly.append(getLabel()).append(": ").append(getName()).append(" ");
+        for (String var : usedVariables) {
+            assembly.append(registerAllocation.getOrDefault(var, var)).append(", ");
+        }
+        assembly.setLength(assembly.length() - 2); // Remove the last comma and space
+        return assembly.toString();
     }
 }
